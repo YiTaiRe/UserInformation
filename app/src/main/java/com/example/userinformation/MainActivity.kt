b46133f4f6934411fa5d686d7f04e29a6b2c68dd
chenyutao <chenyutao@vmos.cn>
2022-09-22 18:29:30 +0800

    1、应用安装界面基本完成

--- app/src/main/java/com/example/userinformation/MainActivity.kt
+++ app/src/main/java/com/example/userinformation/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.userinformation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //获取手机密度
+        val dm = resources.displayMetrics
+        val a = dm.density
+        Log.d("---------","密度为$a")
     }
 }
